5d5728ab5e41ef3c6c007bbfeb7392e4e4f3db4d
Karlo Taslak <karlo.taslak@srce.hr>
2022-11-09 10:40:31 +0100

    Added Entity Framework and Created Db

---
+++ HotelListing.API/Migrations/20221109093537_UpdatedCountriesAndHotels.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedCountriesAndHotels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Name", "ShortName" },
+                values: new object[] { "Bahamas", "BS" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Name", "ShortName" },
+                values: new object[] { "Jamaica", "JM" });
+        }
+    }
+}
